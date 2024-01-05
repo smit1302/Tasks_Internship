5dd9a03eae56950e7299f82c76546b045343c98d
smit1302 <smitjchoksi28@gmail.com>
2024-01-05 15:42:45 +0530

    toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SearchFilter from './filter';
 import Timer from './timer';
-
+// List of array to be used for inserting data in the searched filter 
 const people=[
   {id: 1, name: 'Smit Choksi' },
   { id: 2, name: 'Darshan Modi' },
@@ -10,9 +10,15 @@ const people=[
   {id: 5, name: 'Meet Patel'},
   {id: 6, name:'Tanish Shah'}
 ]
+
+//Event handler function for the button click
+
+
 const App: React.FC = () => {
   const [inputSeconds, setInputSeconds] = useState('');
   const [startTimer, setStartTimer] = useState(false);
+  //State to track whether the button is clicked
+  const [buttonClicked,setButtonClicked]=useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputSeconds(event.target.value);
@@ -21,6 +27,11 @@ const App: React.FC = () => {
   const handleStartTimer = () => {
     setStartTimer(true);
   };
+
+  const handleButtonClick =()=>{
+    setButtonClicked(true);
+  };
+
   return (
     <div>
       <h1>Timer App</h1>
@@ -34,7 +45,11 @@ const App: React.FC = () => {
       />
       <button onClick={handleStartTimer}>Start Timer</button>
       {startTimer && <Timer initialSeconds={parseInt(inputSeconds, 10) || 0} />}
-      <SearchFilter people={people} />;
+      <SearchFilter people={people} />
+      <h1>Toggle Button</h1>
+      <button onClick={handleButtonClick}>
+        {buttonClicked? 'Button Clicked ':'Click me'}
+        </button>
     </div>
   );
 };
